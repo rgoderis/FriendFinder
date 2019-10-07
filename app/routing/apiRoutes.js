d155b6b3c03f995b775b8629096e0cc798ba4443
Ryan Goderis <rgoderis789@gmail.com>
2019-10-07 15:21:20 -0400

    required frinds.js to apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,10 @@
+var friends = require("../data/friends");
+
 module.exports = function(app){
     app.get("/api/friends", function(req, res){
 
     });
     app.post("/api/friends", function(req, res){
-        
-    })
+
+    });
 }
