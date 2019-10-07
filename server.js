9d13f62af50651a29fc28edcd68a92ab524e3b7f
Ryan Goderis <rgoderis789@gmail.com>
2019-10-07 15:02:19 -0400

    added express middleware and app.listen

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-var express = require("express");
-var path = require("path");
-var app = express();
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.statis("public"));
+
+
+
+app.listen(PORT, function(){
+    console.log("Listening on port " + PORT);
+
+});
