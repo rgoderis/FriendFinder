8196ca5d0e4160a73185c3b288f96059cd730621
Ryan Goderis <rgoderis789@gmail.com>
2019-10-08 19:59:52 -0400

    created function compareScores to loop through friends array and compare each score to the user score and return the closest match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,20 +6,48 @@ module.exports = function(app){
     });
     app.post("/api/friends", function(req, res){
         var newFriend = req.body
-        var score = [] 
+        var score = [];
         for(var i = 0; i < newFriend.scores.length; i++){
             var newScore = parseInt(newFriend.scores[i])
             score.push(newScore)
         }
-        console.log(newFriend)
-        console.log(score)
+        // run logic to compare scores
+        var friendIndex = 0
+        var matchName;
+        var matchImage;
+        var matchScore = 100;
+        // function to compare each friends score to score of user
+        function compareScores(ind){
+            // conditional to see if there are any more friends left in the friends array
+            if(ind === friends.length){
+                console.log("You ran out of friends")
+            } else {
+                // compare score of current friend to user
+                console.log(friends[ind].scores)
+                var scoreArr = friends[ind].scores
+                var currentScore = 0
+                for(var i = 0; i < scoreArr.length; i++){
+                    currentScore += Math.abs(scoreArr[i] - score[i]); 
+                }
+                console.log(currentScore)
+                if(currentScore < matchScore){
+                    matchScore = currentScore;
+                    matchImage = friends[ind].photo;
+                    matchName = friends[ind].name
+                    currentScore = 0
+                    ind ++
+                } else {
+                    currentScore = 0
+                    ind ++
+                }
+                compareScores(ind)
+                console.log(matchScore)
+                console.log(matchImage)
+                console.log(matchName)
+            }
+        }
+        compareScores(friendIndex);
         newFriend.scores = score
         friends.push(newFriend);
-        // run logic to compare scores
-        // loop through array of friends
-        // loop through the score array of each friend
-        // perform math logic to find closest score
-        // retrieve match
-        // push addedFriend to friends array
     });
 }
