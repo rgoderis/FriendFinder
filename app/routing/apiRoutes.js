5aa711bfd2eeac8c8b41bcd97c5d50bcbc84e8d7
Ryan Goderis <rgoderis789@gmail.com>
2019-10-09 12:21:58 -0400

    set data to retrieve from app/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,6 +15,7 @@ module.exports = function(app){
         var friendIndex = 0
         var matchName;
         var matchImage;
+        var match;
         var matchScore = 100;
         // function to compare each friends score to score of user
         function compareScores(ind){
@@ -23,17 +24,16 @@ module.exports = function(app){
                 console.log("You ran out of friends")
             } else {
                 // compare score of current friend to user
-                console.log(friends[ind].scores)
                 var scoreArr = friends[ind].scores
                 var currentScore = 0
                 for(var i = 0; i < scoreArr.length; i++){
                     currentScore += Math.abs(scoreArr[i] - score[i]); 
                 }
-                console.log(currentScore)
                 if(currentScore < matchScore){
                     matchScore = currentScore;
                     matchImage = friends[ind].photo;
                     matchName = friends[ind].name
+                    match = friends[ind]
                     currentScore = 0
                     ind ++
                 } else {
@@ -41,13 +41,12 @@ module.exports = function(app){
                     ind ++
                 }
                 compareScores(ind)
-                console.log(matchScore)
-                console.log(matchImage)
-                console.log(matchName)
+
             }
         }
         compareScores(friendIndex);
-        newFriend.scores = score
+        res.json(match);
+        newFriend.scores = score;
         friends.push(newFriend);
     });
 }
